94fea9b2c9e40d774dcd4de8876589fb7e772711
mtatiPL <mateusz-biernat@tlen.pl>
2022-09-15 08:46:43 +0200

    kolekcje

--- src/com/java/Main.java
+++ src/com/java/Main.java
@@ -1,4 +1,8 @@
 package com.java;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
         //losowanie 6 liczb zapiszemy je w tablicy potem w kolekcji
@@ -6,46 +10,24 @@ public class Main {
         //sprawdzamy ile trafionych
         System.out.println("witaj na losowaniu 6 liczb");
         //tablica wylosowane
-        int wylosowane[] = new int[6];
+
         //wartości w tablicy typu prostego lub złożonego
         //w tablicy nie ma możliwości zmiany jej rozmiaru po jej deklaracji
-        for (int i = 0; i < wylosowane.length; i++) {
-            wylosowane[i] = (int) (Math.random() * 100 + 1);
 
+//kolekcje mogą mieć tylko typy  złożone
+        //po zadeklarowaniu można usuwać i dodawac elementy
+        Set<Integer> wylosowane = new HashSet<>();
+        //zbiór zazwyczaj zawiera elementy bez powtórzeń
+        //zbiór zazwyczaj nie ma indeksowanych elementów
+        //gdy używamy for ale są powtórzenia to będzie mniej niż 6 a używając while będzie się wykonywać tak długo aż będzie 6 różnych liczb
+
+        while (wylosowane.size() < 6) {
+            wylosowane.add((int) (Math.random() * 10 + 1));
         }
         for (int element : wylosowane) {
             System.out.print(element + ", ");
         }
 
-        int wylosowana2[];
-        wylosowana2 = wylosowane;
-        wylosowane[0] = 0;
-        System.out.println();
-        System.out.println("tablica 1");
-        for (int element : wylosowane) {
-            System.out.print(element + ", ");
-        }
-        System.out.println("tablica 2");
-        for (int element : wylosowana2) {
-            System.out.print(element + ", ");
-        }
-        System.out.println(wylosowane);
-        System.out.println(wylosowana2);
-        int wylosowane3[];
-        wylosowane3 = wylosowane.clone();
-        wylosowane[1] = 0;
-        System.out.println("tablica 1");
-        for (int element : wylosowane) {
-            System.out.print(element + ", ");
-        }
-        System.out.println("tablica 2");
-        for (int element : wylosowana2) {
-            System.out.print(element + ", ");
-        }
-        System.out.println("tablica 3");
-        for (int element : wylosowane3) {
-            System.out.print(element + ", ");
-        }
 
     }
-    }
+}
