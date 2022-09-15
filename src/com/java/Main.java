4e2d96aaa0a707095c872faba84bd57202e28fde
mtatiPL <mateusz-biernat@tlen.pl>
2022-09-15 08:10:57 +0200

    wylosowanie tablicy

--- src/com/java/Main.java
+++ src/com/java/Main.java
@@ -1,6 +1,21 @@
 package com.java;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("");
+        //losowanie 6 liczb zapiszemy je w tablicy potem w kolekcji
+        //wypisywanie 6 liczb
+        //sprawdzamy ile trafionych
+        System.out.println("witaj na losowaniu 6 liczb");
+        //tablica wylosowane
+        int wylosowane[]=new int [6];
+        //wartości w tablicy typu prostego lub złożonego
+        for(int i=0;i<wylosowane.length;i++){
+            wylosowane[i]=(int)(Math.random()*100+1);
+
+        }
+        for (int element:wylosowane){
+            System.out.print(element+", ");
+        }
+
+
     }
 }
